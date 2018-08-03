950419cbbb65caa6b47655e688b99a85ef3e6f98
gadereshma5 <38101853+gadereshma5@users.noreply.github.com>
2018-08-03 17:38:53 +0530

    selenium hybrid framework

--- Actions.java
+++ Actions.java
@@ -1,70 +1,153 @@
-package KeywordDriven;
+package HybridFramework;
  
 import org.openqa.selenium.chrome.ChromeDriver;
-
- 
+import org.testng.Reporter;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
- 
-public class Actions 
+
+
+
+
+public class Actions extends ReadExcelData
 {
  public static WebDriver driver;
-  
  
  
- public static void openBrowser()
- { 
- System.setProperty("webdriver.chrome.driver", "E:\\Project\\SeleniumProj\\Driver\\chromedriver.exe");
- driver=new ChromeDriver();
- 
- }
-  
- public static void navigate(String site)
- { 
- driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
- driver.get(site);
- }
-  
- public static void type(String cmd,String cmd1)
+ public static void captureScreenshot(WebDriver driver,String screenshotName) throws IOException
  {
-	 String input=cmd;
-	 String[] xpath=input.split("=");
-	// System.out.println(xpath[0]);
-	// System.out.println(xpath[1]);
-	 
-	 //System.out.println(" "+cmd);
-	 //System.out.println(" "+cmd1);
-	 
-	
-	
-	 if(xpath[0].equals("name"))
-	 {
-		 driver.findElement(By.name(xpath[1])).sendKeys(cmd1);
-	 }else if(xpath[0].equals("id"))
-	 {
-		 driver.findElement(By.id(xpath[1])).sendKeys(cmd1);
-	 }
- }
- 
- public static void click(String clk) throws InterruptedException
- {
-	
-	 String input=clk;
-	 String[] xpath1=input.split("=");
-	 System.out.println("XPATH1" + xpath1[0]);
-	 System.out.println("XPATH1" + xpath1[1]);
-	 System.out.println("XPATH1" + xpath1[2]);
-	 Thread.sleep(500);
-	 //driver.findElement(By.className("nav-input")).click();
-	 
-	driver.findElement(By.xpath(xpath1[1]+"="+xpath1[2])).click();
- 
+
+ TakesScreenshot ts=(TakesScreenshot)driver;
+  
+ File source=ts.getScreenshotAs(OutputType.FILE);
+  
+ FileUtils.copyFile(source, new File("E:\\framework\\"+screenshotName+".png"));
+  
+ System.out.println("Screenshot taken");
+
  }
 
+ 
+ public static void openBrowser() throws IOException
+ { 
+
+	 try {
+		System.setProperty("webdriver.chrome.driver", "E:\\Project\\SeleniumProj\\Driver\\chromedriver.exe");
+		 driver=new ChromeDriver();
+		 Reporter.log("Browser Opened");
+		 logger.info("Browser Opened");
+		 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		 driver.manage().window().maximize();
+		 // Reporter.log("Browser Maximized");
+          logger.info("Browser Maximized");
+
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		captureScreenshot(driver,"openbrowser");
+	}
+	
+}
+
+
+ public static void navigate(String site) throws IOException
+ { 
+
+	 try {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		 driver.get(site);
+
+	       // Reporter.log("Application started");
+	        logger.info("Application started");
+		 
+	} catch (Exception e) {
+		
+		e.printStackTrace();
+		captureScreenshot(driver,"openbrowser");
+	}
+}
+
+
+ public static void type(String cmd,String cmd1) throws IOException
+ {
+		
+			  try {
+				  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+				  driver.manage().window().maximize();
+				  String input=cmd;
+				  String[] xpath=input.split("=");
+				// System.out.println(xpath[0]);
+				// System.out.println(xpath[1]);
+				 
+				 //System.out.println(" "+cmd);
+				 //System.out.println(" "+cmd1);
+				  //logger.info("taking input");
+					if(xpath[0].equals("id"))
+					 {
+						 driver.findElement(By.id(xpath[1])).sendKeys(cmd1);
+						 
+					 }else if(xpath[0].equals("xpath"))
+					 {
+						 driver.findElement(By.xpath(xpath[1]+"="+xpath[2])).sendKeys(cmd1);
+						 
+					 }else if(xpath[0].equals("name"))
+					 {
+						 driver.findElement(By.name(xpath[1])).sendKeys(cmd1);
+					 }
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				captureScreenshot(driver,"type");
+			}
+
+				
+
+	}
+	 
+	
+ 
+
+ public static void click(String clk) throws InterruptedException, IOException
+ {
+	
+	 try {
+		 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		  driver.manage().window().maximize();
+		String input=clk;
+		 String[] xpath1=input.split("=");
+		 System.out.println("XPATH1" + xpath1[0]);
+		 System.out.println("XPATH1" + xpath1[1]);
+		 System.out.println("XPATH1" + xpath1[2]);
+		 Thread.sleep(500);
+		// logger.info("click action");
+		  if(xpath1[0].equals("id"))
+			 {
+				 driver.findElement(By.id(xpath1[1])).click();
+			 }
+		  else if(xpath1[0].equals("xpath"))
+		  {
+		  driver.findElement(By.xpath(xpath1[1]+"="+xpath1[2])).click();
+		  }
+		  
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		captureScreenshot(driver,"click");
+	}
+	 
+ }
+ 
  public static void closeBrowser()
  {
 	 driver.quit();
+	 //Reporter.log("Application closed");
+	 logger.info("Application closed");
  }
 
 
--- ReadExcelData.java
+++ ReadExcelData.java
@@ -2,21 +2,22 @@ package HybridFramework;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.LinkedHashMap;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-import KeywordDriven.Actions;
-
-class readvalues{
+class ReadExcelData  {
 	// static LinkedHashMap<String,String> hm=new LinkedHashMap<String,String>();
-	 
+	static Logger logger =  Logger.getLogger("ReadExcelData");
+	
 	void readtestsuit() throws IOException, InterruptedException
 	{
+		PropertyConfigurator.configure("Log4j.properties");
 		File testsuit_filepath=new File("E:\\framework\\testsuit.xlsx");
 		FileInputStream fis=new FileInputStream(testsuit_filepath);
 		XSSFWorkbook wb=new XSSFWorkbook(fis);
@@ -25,125 +26,121 @@ class readvalues{
 		System.out.println("row count in testsuit sheet "+rowcount);
 		
 		for(int i=1;i<=rowcount;i++) {
+
 		sheet1.getRow(i);
 		String data =sheet1.getRow(i).getCell(0).getStringCellValue();
 		String data1 =sheet1.getRow(i).getCell(1).getStringCellValue();
 		
-		//System.out.println(data+ " " + data1);
+		System.out.println(data+ " " + data1);
 		readscript(data,data1);
 		
 		}
-
+		
 		
 	}
 	
-void objectexcel() throws IOException
-{
-	File object_filepath=new File("E:\\framework\\object.xlsx");
-	FileInputStream fis2=new FileInputStream(object_filepath);
-	XSSFWorkbook wb2=new XSSFWorkbook(fis2);
-	XSSFSheet objsheet=wb2.getSheetAt(0);
-	int noofsheets= wb2.getNumberOfSheets();
-	System.out.println("sheets in object files: "+noofsheets);
-	
-	
-	for(int i=0;i<noofsheets;i++)
-	{
-		String sheetname1=wb2.getSheetName(i);
-		System.out.println("SheetName"+ sheetname1 );
-	
-	}
-	
-	
-}
 	
  void  readscript(String data, String data1) throws IOException, InterruptedException
 	{
-	 		
-	    objectexcel();
-	//	System.out.println(data+ "   " +data1);
+	 	System.out.println("function called");
+	    //objectexcel();
+	    //System.out.println(data+ "   " +data1);
 		File script_filepath=new File("E:\\framework\\testscript.xlsx");
 		FileInputStream fis1=new FileInputStream(script_filepath);
 		XSSFWorkbook wb1=new XSSFWorkbook(fis1);
 		XSSFSheet sheet2=wb1.getSheetAt(0);
 		int noofsheets=wb1.getNumberOfSheets();
-		//System.out.println(noofsheets);
+		System.out.println(noofsheets);
 		int rowcount1=sheet2.getLastRowNum()-sheet2.getFirstRowNum();
+		System.out.println("rows in script sheet "+rowcount1);
+		//String value=sheet2.getRow(1).getCell(1).getStringCellValue(); 
+		// System.out.println("cell value in script sheet "+ value);
 		
-		String value=sheet2.getRow(1).getCell(1).getStringCellValue(); 
-		 System.out.println("cell value in script sheet "+ value);
 		
-		
-			for(int i=0;i<noofsheets;i++)
-			{	
+		for(int i=0;i<noofsheets;i++)
+		{	
+			 //XSSFSheet sheetname=wb1.getSheetName(i);
+				System.out.println("Inside For loop");
+				XSSFSheet sheetname=wb1.getSheetAt(i);
+				String scriptsheetname=wb1.getSheetName(i);
 				
-				String sheetname=wb1.getSheetName(i);
-				//System.out.println("SheetName"+ sheetname );
-				//System.out.println("value of data" + data);
-				//System.out.println("value of data1"+ data1);
-				
-				//if(data=="Y" && data1.equals(sheetname))
-				if(data.equals("Y") && data1.equals(sheetname))
+				if(data.equals("Y") && data1.equals(scriptsheetname))
 				{
 					System.out.println("Equals");
+					show(sheetname);
+					
 				}
-				else
-				{
-					System.out.println("not Equals");
-				}
-			}
-			
-		
-			for(int j=1;j<=rowcount1;j++) 
-			{
 				
-			 String cell =sheet2.getRow(j).getCell(0).getStringCellValue();
-			// System.out.println(cell);
-			 String cell1=sheet2.getRow(j).getCell(2).getStringCellValue();
-			// System.out.println(cell1);
-			 String cell2=sheet2.getRow(j).getCell(3).getStringCellValue();
+		}
+			
+	}
+
+	public static void show(XSSFSheet sheetname) throws InterruptedException, IOException
+
+		{
+			
+			System.out.println("SHOW FUNCTION");
+			XSSFSheet scriptsheetname=sheetname;
+			//System.out.println("SheetMame" + sheetname);
+			
+			
+			//System.out.println("Inside loop******");	
+			
+			int rowcount1=scriptsheetname.getLastRowNum()-scriptsheetname.getFirstRowNum();
+			System.out.println("****RowCount" + rowcount1);
+			
+			for(int i=2;i<=rowcount1;i++) 
+			{
+			
+			
+			 String cell =scriptsheetname.getRow(i).getCell(0).getStringCellValue();
+			 System.out.println(cell);
+			 String cell1=scriptsheetname.getRow(i).getCell(2).getStringCellValue();
+			 System.out.println(cell1);
+			 String cell2=scriptsheetname.getRow(i).getCell(3).getStringCellValue();
+			 //System.out.println(cell2);
+			// XSSFCell cell2=scriptsheetname.getRow(i).getCell(3);
 			// System.out.println(cell2);
 			 
-			if(cell.equals("openBrowser"))
-			{
-				Actions.openBrowser();
-				
-			}else if(cell.equals("navigate")) 
-			{
-				Actions.navigate(cell1);
-				
-			}else if(cell.equals("type"))
-			{
-				Actions.type(cell1, cell2);
-				
-			}else if(cell.equals("click"))
-			{
-				Actions.click(cell1);
-				
-			}else if(cell.equals("closeBrowser"))
-			{
-				Actions.closeBrowser();
-			}
+			
+				 
+			 
+			 if(cell.equals("openBrowser"))
+		 		{
+		 			Actions.openBrowser();
+			
+		 		}else if(cell.equals("navigate")) 
+		 		{
+		 			Actions.navigate(cell1);
+		 		}
+			
+		 		else if(cell.equals("type"))
+		 		{
+		 			
+		 			//String input=scriptsheetname.getRow(i).getCell(3).getStringCellValue();
+		 			Actions.type(cell1, cell2);
+			
+		 		}else if(cell.equals("click"))
+		 		{
+		 			Actions.click(cell1);
+		 				
+		 		}else if(cell.equals("closeBrowser"))
+		 		{
+		 			Actions.closeBrowser();
+		 		}
+			 }
 			
 			
-		
-		}
-		
-		
-	}
+	    }
  
-}
 		
-	
-
-
-public class ReadExcelData {
-	public static void main(String args[]) throws IOException, InterruptedException
+		@Test(priority=1)
+	public static void get() throws IOException, InterruptedException
 	{
-		readvalues object=new readvalues();
 		
+		System.out.println("Inside Main");
+		ReadExcelData object=new ReadExcelData();
 		object.readtestsuit();
-		
 		
 	}
 
